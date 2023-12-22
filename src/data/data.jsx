e8cefe779d859f09d4da8bb79fd15375df52a556
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-12-22 15:17:04 +0530

    borad added

---
+++ src/data/data.jsx
@@ -0,0 +1,70 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import amazonPster from "../assets/projects/amazonClone/amazonPoster.png"
+import stopwatch from "../assets/projects/amazonClone/stopwatch.png"
+import stpotify from "../assets/projects/amazonClone/sptify.png"
+import gym from "../assets/projects/gym.png"
+import cara from "../assets/projects/cara.png"
+export const data=[
+    {
+        id:0,
+        name:"Amazon_clone",
+        image:amazonPster,
+        github:"https://github.com/shailja026/amezon_clone.git",
+        live:"https://amezon-clone-d455.vercel.app/",
+       
+    },
+    
+    {
+        id:1,
+        name:"Gym_website",
+        image:gym,
+        github:"https://github.com/shailja026/Gym_website/tree/master",
+        live:"https://github.com/shailja026/pretty_fit.git",
+    },
+    {
+        id:7,
+        name:"Cara",
+        image:cara,
+        github:"https://github.com/shailja026/Cara/tree/master/E-commerce",
+        live:"https://e-commerce-clone-gd2.netlify.app/",
+    },
+   
+    {
+        id:4,
+        name:"Movie_Land",
+        image:RealEst,
+        github:"https://github.com/shailja026/movies_with_api",
+        live:"",
+    },
+    {
+        id:5,
+        name:"Stopwatch",
+        image:stopwatch,
+        github:"https://github.com/shailja026/stopWatch.git",
+        live:"https://stop-watch-topaz.vercel.app/",
+    },
+    {
+        id:6,
+        name:"Sptify_clone",
+        image:stpotify,
+        github:"https://github.com/shailja026/music_app.git",
+        live:"https://music-app-six-taupe.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Twitter_clone",
+        image:RealEst,
+        github:"https://github.com/shailja026/abcd/tree/tweet",
+        live:"https://peaceful-gaufre-7ba2da.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Kanban-board ",
+        image:WorkIm,
+        github:"https://github.com/shailja026/kanban-board",
+        live:"https://reliable-raindrop-066f2f.netlify.app/",
+    }
+
+
+]
